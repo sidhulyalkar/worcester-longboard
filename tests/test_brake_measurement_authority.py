9a50be27c6ad15b27c7ae2524ed9dcfb9f4c2049
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-09-11 11:35:10 -0700

    test: bind brake torque to measured wheel geometry

--- tests/test_brake_measurement_authority.py
+++ tests/test_brake_measurement_authority.py
@@ -1,5 +1,4 @@
 import argparse
-from copy import deepcopy
 
 from tools.init_brake_measurement_session import build_manifest
 from tools.qualify_brake_interface import qualify
@@ -14,8 +13,24 @@ def _args():
 
 def _passing_manifest():
     m = build_manifest(_args())
-    for key in m["measurements_mm"]:
-        m["measurements_mm"][key] = 10.0
+    m["measurements_mm"].update({
+        "rotor_outer_diameter": 160.0,
+        "rotor_inner_diameter": 80.0,
+        "rotor_thickness": 2.0,
+        "rotor_bolt_circle_diameter": 120.0,
+        "rotor_to_hub_axial_offset": -4.0,
+        "brake_arm_pivot_thread_major_diameter": 8.0,
+        "brake_arm_pivot_axis_to_axle_x": -18.0,
+        "brake_arm_pivot_axis_to_axle_z": 12.0,
+        "spring_pin_offset_x": 0.0,
+        "spring_pin_offset_z": 9.0,
+        "pad_radial_center": 70.0,
+        "released_arm_max_extent": 42.0,
+        "applied_arm_max_extent": 47.0,
+        "cable_min_bend_radius": 35.0,
+        "inflated_tire_max_diameter": 220.0,
+        "inflated_tire_max_width": 55.0,
+    })
     for key in m["checks"]:
         m["checks"][key] = True
     m["static_brake_test"].update({
@@ -40,6 +55,13 @@ def test_complete_unpowered_evidence_can_verify_brake_interface():
     assert report["powered_operation_authorized"] is False
 
 
+def test_signed_and_zero_datums_are_allowed():
+    m = _passing_manifest()
+    m["measurements_mm"]["rotor_to_hub_axial_offset"] = -6.0
+    m["measurements_mm"]["spring_pin_offset_x"] = 0.0
+    assert qualify(m)["qualified"] is True
+
+
 def test_missing_geometry_blocks_qualification():
     m = _passing_manifest()
     m["measurements_mm"]["rotor_outer_diameter"] = None
@@ -48,12 +70,29 @@ def test_missing_geometry_blocks_qualification():
     assert any("rotor_outer_diameter" in err for err in report["errors"])
 
 
+def test_impossible_rotor_geometry_blocks_qualification():
+    m = _passing_manifest()
+    m["measurements_mm"]["rotor_inner_diameter"] = 170.0
+    report = qualify(m)
+    assert report["qualified"] is False
+    assert any("outer diameter must exceed" in err for err in report["errors"])
+
+
 def test_bad_torque_consistency_blocks_qualification():
     m = _passing_manifest()
     m["static_brake_test"]["brake_torque_nm"] = 25.0
     report = qualify(m)
     assert report["qualified"] is False
     assert any("torque inconsistent" in err for err in report["errors"])
+
+
+def test_wrong_wheel_radius_blocks_qualification():
+    m = _passing_manifest()
+    m["static_brake_test"]["wheel_radius_m"] = 0.15
+    m["static_brake_test"]["brake_torque_nm"] = 15.0
+    report = qualify(m)
+    assert report["qualified"] is False
+    assert any("wheel radius inconsistent" in err for err in report["errors"])
 
 
 def test_brake_authority_never_promotes_power():
